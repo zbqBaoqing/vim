c0b9d185c739deae4061de4c427cab551f97dd3a
zbqyexingkong <abqyexingkoong@gmail.com>
2013-11-19 18:30:11 +0800

    快速 编辑-编译-编辑循环

---
+++ quickfix/build_vs10.bat
@@ -0,0 +1,15 @@
+echo off
+set COMMAND=%1
+
+set PROFILE=%2
+
+set VSPATH=%3
+
+if "%1" == "" set COMMAND=build
+
+if "%2" == "" set PROFILE=Release
+
+devenv quickfix_vs10.sln /%COMMAND% %PROFILE%
+
+if ERRORLEVEL 1 goto quit
+:quit
