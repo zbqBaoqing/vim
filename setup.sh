ec31e0dfb35b6e0dda1c346b63711eaff9e878fd
yexingkong <abqyexingkong@gmail.com>
2014-05-08 21:39:49 +0800

    快速安装

--- setup.sh
+++ setup.sh
@@ -3,13 +3,13 @@ echo "安装将花费一定时间，请耐心等待直到安装完成^_^"
 if which apt-get >/dev/null; then
 	sudo apt-get install -y vim vim-gnome ctags  git 
 elif which yum >/dev/null; then
-	sudo yum install -y gcc vim git ctags 	
+	sudo yum install -y gcc vim git ctags vim-gnome	
 fi
 
 ##Add HomeBrew support on  Mac OS
 if which brew >/dev/null;then
     echo "You are using HomeBrew tool"
-    brew install vim ctags git astyle
+    brew install vim ctags git
 fi
 
 cd /usr/include/ && sudo ctags -R --fields=+ls 
@@ -20,7 +20,7 @@ cd ~/ && git clone https://github.com/zbqyexingkong/vim.git
 mv -f ~/vim/.vimrc ~/
 mv -f ~/vim/* ~/.vim/bundle/
 rm -fr ~/vim/
-echo "zbqyexingkong正在努力为您安装bundle程序";
+echo "zbqyexingkong正在努力为您安装bundle程序"
 echo "安装完毕将自动退出" >> zbqyexingkong
 echo "请耐心等待" >> zbqyexingkong
 vim zbqyexingkong -c "BundleInstall" -c "q" -c "q"
